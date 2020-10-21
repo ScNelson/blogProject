0416bf99710d714f98ddad2577182f47d8c36e6d
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-10-21 09:38:18 -0400

    Added annotations

--- src/main/java/com/tts/techtalentblog/BlogPost/BlogPost.java
+++ src/main/java/com/tts/techtalentblog/BlogPost/BlogPost.java
@@ -1,6 +1,15 @@
 package com.tts.techtalentblog.BlogPost;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class BlogPost {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id; 
     private String title;
     private String author;
@@ -45,8 +54,7 @@ public class BlogPost {
     @Override
     public String toString() {
         return "{" +
-            " id='" + getId() + "'" +
-            ", title='" + getTitle() + "'" +
+            " title='" + getTitle() + "'" +
             ", author='" + getAuthor() + "'" +
             ", blogEntry='" + getBlogEntry() + "'" +
             "}";
