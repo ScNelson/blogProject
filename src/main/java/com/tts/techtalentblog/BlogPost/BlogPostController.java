b2666812c027e978ebb5367540e8f1bac00ea873
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-10-22 09:56:29 -0400

    Add method for deleting blog posts

--- src/main/java/com/tts/techtalentblog/BlogPost/BlogPostController.java
+++ src/main/java/com/tts/techtalentblog/BlogPost/BlogPostController.java
@@ -7,7 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BlogPostController {
@@ -43,5 +46,11 @@ public class BlogPostController {
 	    model.addAttribute("blogEntry", blogPost.getBlogEntry());
         
         return "blogpost/result";
+    }
+
+    @RequestMapping(value = "/blogpost/{id}", method = RequestMethod.DELETE)
+    public String deletePostWithID(@PathVariable Long id, BlogPost blogPost) {
+        blogPostRepository.deleteById(id);
+        return "/blogpost/index";
     }
 }
