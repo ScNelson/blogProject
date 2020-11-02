1fe5db27a7e805bf66ef24f823050255d78e508d
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-11-01 20:21:45 -0500

    Added finding post by tag phrase method

--- src/main/java/com/tts/techtalentblog/BlogPost/BlogPostRepository.java
+++ src/main/java/com/tts/techtalentblog/BlogPost/BlogPostRepository.java
@@ -1,7 +1,9 @@
 package com.tts.techtalentblog.BlogPost;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface BlogPostRepository extends CrudRepository<BlogPost, Long> {
-    
+    List<BlogPost> findByTags_Phrase(String phrase);
 }
