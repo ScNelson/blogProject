48f9c4ca03fa0727470826f3b5006ba3e80151f4
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-11-01 20:21:03 -0500

    Added tag/tags attributes

--- src/main/java/com/tts/techtalentblog/BlogPost/BlogPost.java
+++ src/main/java/com/tts/techtalentblog/BlogPost/BlogPost.java
@@ -1,9 +1,16 @@
 package com.tts.techtalentblog.BlogPost;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class BlogPost {
@@ -12,19 +19,31 @@ public class BlogPost {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id; 
     private String title;
-    private String author;
     private String topic;
+    private String author;
     private String blogEntry;
+
+    private String tag;
+    
+    @ManyToMany(fetch = FetchType.LAZY, cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+	@JoinTable(name = "post_tag", joinColumns = @JoinColumn(name = "post_id"), inverseJoinColumns = @JoinColumn(name = "tag_id"))
+    private List<Tag> tags;
+
 
     public BlogPost() {
     }    
 
-    public BlogPost(String title, String author, String topic, String blogEntry) {
+
+
+    public BlogPost(Long id, String title, String topic, String author, String blogEntry, String tag) {
+        this.id = id;
         this.title = title;
-        this.author = author;
         this.topic = topic;
+        this.author = author;
         this.blogEntry = blogEntry;
+        this.tag = tag;
     }
+    
 
     public Long getId() {
         return id;
@@ -62,14 +81,32 @@ public class BlogPost {
         this.blogEntry = blogEntry;
     }
 
+    public String getTag() {
+        return this.tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public List<Tag> getTags() {
+        return this.tags;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
 
     @Override
     public String toString() {
         return "{" +
-            " title='" + getTitle() + "'" +
+            " id='" + getId() + "'" +
+            ", title='" + getTitle() + "'" +
+            ", topic='" + getTopic() + "'" +
             ", author='" + getAuthor() + "'" +
-            ", subHeader='" + getTopic() + "'" +
             ", blogEntry='" + getBlogEntry() + "'" +
+            ", tag='" + getTag() + "'" +
             "}";
     }
+
 }
